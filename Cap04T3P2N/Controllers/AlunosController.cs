0ef58b98a997e0c38a2567d0b11e24ff2f15efc0
daniel-alencar-br <danielalencar.br@gmail.com>
2022-09-13 22:39:59 -0300

    Info Conexao

--- Cap04T3P2N/Controllers/AlunosController.cs
+++ Cap04T3P2N/Controllers/AlunosController.cs
@@ -7,6 +7,9 @@ using Cap04T3P2N.Dados;
 
 namespace Cap04T3P2N.Controllers
 {
+    // data source=PC_DO_GAB;   ===>   data source=MAQPESSOAL\sqlexpress;
+    // user id=sa;password=sa;  ===>   integrated security=sspi;
+
     public class AlunosController : Controller
     {
         public ActionResult Index()
