631473a60453c478d8dd73f56d71b1b22c1cb521
daniel-alencar-br <danielalencar.br@gmail.com>
2022-09-15 21:37:03 -0300

    Operaoes CRUD

--- Cap04T3P2N/Controllers/AlunosController.cs
+++ Cap04T3P2N/Controllers/AlunosController.cs
@@ -12,11 +12,13 @@ namespace Cap04T3P2N.Controllers
 
     public class AlunosController : Controller
     {
+        // site.com.br/alunos/index
         public ActionResult Index()
         {
             return View();
         }
 
+        // site.com.br/alunos/listar
         public ActionResult Listar()
         {
             try
@@ -29,8 +31,94 @@ namespace Cap04T3P2N.Controllers
                 return View("_Erro");
             }
         }
+
+        [HttpGet]
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(Models.Alunos Novo)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                AlunosCRUD.NovoAluno(Novo);
+                return RedirectToAction("Listar");
+            }
+            catch (Exception erro)
+            {
+                ViewBag.Mensagem = erro.Message;
+                return View("_Erro");
+            }
+        }
+
+        private ActionResult BuscarAluno(int Codigo, string Operacao)
+        {
+            try
+            {
+                var InfoAluno = AlunosCRUD.BuscarAluno(Codigo);
+                return View(Operacao, InfoAluno);
+            }
+            catch (Exception erro)
+            {
+                ViewBag.Mensagem = erro.Message;
+                return View("_Erro");
+            }
+        }
+        // site.com.br/alunos/alterar/2
+        public ActionResult Alterar(int id)
+        {
+            return BuscarAluno(id, "Alterar");
+        }
+        // site.com.br/alunos/Pesquisar/2
+        public ActionResult Pesquisar(int id)
+        {
+            return BuscarAluno(id, "Pesquisar");
+        }
+        // site.com.br/alunos/apagar/2
+        public ActionResult Apagar(int id)
+        {
+            return BuscarAluno(id, "Apagar");
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Models.Alunos Aluno)
+        {
+            try
+            {
+                AlunosCRUD.AlterarAluno(Aluno);
+                return RedirectToAction("Listar");
+            }
+            catch (Exception erro)
+            {
+                ViewBag.Mensagem = erro.Message;
+                return View("_Erro");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Apagar(Models.Alunos Aluno)
+        {
+            try
+            {
+                AlunosCRUD.DeletarAluno(Aluno);
+                return RedirectToAction("Listar");
+            }
+            catch (Exception erro)
+            {
+                ViewBag.Mensagem = erro.Message;
+                return View("_Erro");
+            }
+        }
     }
 }
+
+
+
 
 
 
